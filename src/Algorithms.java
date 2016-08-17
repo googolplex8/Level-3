98c5d2f95ce0044622394738a69f18a0c6c1783c
Your Name <you@example.com>
2016-08-16 19:55:36 -0700

    finished last 3 algorithms

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Algorithms {
@@ -22,38 +23,109 @@ public class Algorithms {
 		}
 		return num;
 	}
-	
-	
-	static Double findTallest(List<Double> peeps){
+
+	static Double findTallest(List<Double> peeps) {
 		Double tallest = 0.0;
-		for (Double x: peeps){
-			if(x>tallest){
-				tallest =x;
+		for (Double x : peeps) {
+			if (x > tallest) {
+				tallest = x;
 			}
 		}
 		return tallest;
 	}
-	static String findLongestWord(List<String> words){
+
+	static String findLongestWord(List<String> words) {
 		int longest = 0;
 		String longestWord = "";
-		for(String x : words){
-			if(x.length()>longest){
+		for (String x : words) {
+			if (x.length() > longest) {
 				longestWord = x;
-				longest=x.length();
+				longest = x.length();
 			}
 		}
 		return longestWord;
 	}
-	static Boolean containsSOS(List<String> message){
+
+	static Boolean containsSOS(List<String> message) {
 		Boolean containsSOS = false;
-		for(String x: message){
-			if(x.contains("... --- ...")){
-			containsSOS = true;
+		for (String x : message) {
+			if (x.contains("... --- ...")) {
+				containsSOS = true;
 			}
 		}
 		return containsSOS;
 	}
-	//static Double sortScores(List<Double> scores){
-		//List <Double> inOrder = new List<Double>();
-	//}
+
+	static ArrayList<Double> sortScores(ArrayList<Double> scores) {
+		Double prior = scores.get(0);
+		int pos = 0;
+		boolean change = true;
+		while (change == true) {
+			change = false;
+			pos = 0;
+			prior = scores.get(0);
+			for (Double x : scores) {
+				if (x >= prior) {
+					prior = x;
+					pos += 1;
+				} else if (x < prior) {
+					scores.set(pos, prior);
+					scores.set(pos - 1, x);
+					pos += 1;
+					change = true;
+				}
+			}
+		}
+		return scores;
+	}
+
+	static ArrayList<String> sortDNA(ArrayList<String> unsorted) {
+		String prior = "";
+		int pos = 0;
+		boolean dif = true;
+		while (dif == true) {
+			dif = false;
+			pos = 0;
+			prior = "";
+			for (String x : unsorted) {
+				if (prior.length() < x.length()) {
+					prior = x;
+					pos += 1;
+				} else if (prior.length() > x.length()) {
+					unsorted.set(pos, prior);
+					unsorted.set(pos - 1, x);
+					pos += 1;
+					dif = true;
+				}
+			}
+		}
+		return unsorted;
+	}
+
+	static ArrayList<String> sortWords(ArrayList<String> words) {
+		String prior = "";
+		int pos = 0;
+		boolean out = true;
+		while (out == true) {
+			prior = "";
+			pos = 0;
+			out = false;
+			for (String x : words) {
+				if (String.CASE_INSENSITIVE_ORDER.compare(prior, x) == -1) {
+					prior = x;
+					pos += 1;
+				} else if (String.CASE_INSENSITIVE_ORDER.compare(prior, x) == 1) {
+					words.set(pos, prior);
+					words.set(pos - 1, x);
+					out = true;
+				} else {
+					prior = x;
+					pos += 1;
+				}
+
+			}
+
+		}
+		return words;
+	}
 }
