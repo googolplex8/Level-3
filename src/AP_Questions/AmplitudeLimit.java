6cd1360e75cfd9ca25fc4ed69e8604cc6d68a30d
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2016-09-23 18:30:37 -0700

    change

--- src/AP_Questions/AmplitudeLimit.java
+++ src/AP_Questions/AmplitudeLimit.java
@@ -1,5 +1,15 @@
 package AP_Questions;
 
 public class AmplitudeLimit {
-
+	int[] values;
+public AmplitudeLimit(int[] values) {
+	this.values=values;
+}
+public int limitAmplitude(int limit){
+	int numChanged = 0;
+	for(int x : values){
+		
+	}
+	return numChanged;
+}
 }
